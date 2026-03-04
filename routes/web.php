de1039a1e78ff507b341c51a4a67e3ebf6833839
ProblemIsMe <soeltanjr01@gmail.com>
2026-03-04 10:52:23 +0700

    login update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/Admin',[AdminController::class, 'view'])->name('Admin');
+    // route logout
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::get('/', [HomeController::class,'view']);
+});
+
 Route::get('/', function () {
     return view('index');
 });
