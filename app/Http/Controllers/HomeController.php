11c00653002e350367da5efcd1c00d80a3c89b5d
Davin Raffilio Barayudha <gulfport30b@gmail.com>
2026-03-05 13:18:00 +0700

    testimoni

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Preview;
 use App\Models\Service;
 use App\Models\Hero;
-<<<<<<< HEAD
 use App\Models\Testimoni;
-=======
-use App\Models\Message;
->>>>>>> a42f84803e08232f1ad60ffc6d63985322ee1c5a
 
 
 class HomeController extends Controller
@@ -31,12 +27,8 @@ class HomeController extends Controller
     public function view()
     {
         $previews = Preview::all();
-<<<<<<< HEAD
         $testimonis = Testimoni::all();
         
-=======
-        $message = Message::all();
->>>>>>> a42f84803e08232f1ad60ffc6d63985322ee1c5a
         $hero = hero::first();
         
         return view('index', [
