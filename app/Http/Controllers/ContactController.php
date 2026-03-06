fb03df0acf8d3d415363443a73340155d2d845e3
ProblemIsMe <soeltanjr01@gmail.com>
2026-03-06 10:23:03 +0700

    error

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,63 +7,34 @@ use App\Models\Contact;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
-class ContactController extends Controller
+class HeroController extends Controller
 {
-    //
      public function view()
     {
           $user= Auth::user();
-        $contacts = Contact::first();
+         $contacts = Contact::first();
         
         return view('contact.edit',[
-            "contacts" => $contacts,
+            "contact" => $contacts,
             "name" => $user['name']
         ]);
     }
 
-    
-
-    public function store(Request $request){
-    
-    $validator = $request->validate([
-        'title' => 'required|string|max:255',
-        'tagline' => 'required|string|max:255',
-        'contact' => 'required|string|max:255',
-        
-    ]);
-
-   
-    
-    $contact = Contact::create($validator);
-
-    return redirect()->route('contact.edit')->with('success', 'Berhasil Menambahkan Data');
-    }
-
-    public function edit(Contact $contact)
-    {
-        $user = Auth::user();
-        return view('contact.edit',[
-        "name" => $user['name'],
-        "contact" => $contact
-
-        ]);
-    }
-
-    public function update (Request $request, Contact $contact){
+     public function update (Request $request, Contact $contact){
            $validator = $request->validate([
              'title' => 'required|string|max:255',
              'tagline' => 'required|string|max:255',
-             'contact' => 'required|string|max:255'
+             'contact' => 'required|string|max:255',
         ]);
-             
+            if ($request->hasFile('image')){
+                if($contact->image && Storage::disk('public')->exists($contact->image)){
+                    Storage::disk('public')->delete($contact->image);
+                }
+                $imagePath = $request->file('image')->store('contact_images', 'public');
+                $validator['image'] = $imagePath;
+            
+            } 
             $contact->update($validator);
-            return redirect()->route('contact.edit')->with('success', 'Berhasil Update Data');
-    }
-
-    public function destroy(Contact $contact){
-            
-            $contact->delete();
-            
-            return redirect()->route('contact.edit')->with('success', 'Berhasil menghapus Data');
+            return redirect()->route('contact.edit')->with('success', 'Berhasil');
     }
 }
